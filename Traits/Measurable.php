afea044d4aac92a17dfd988cdaacc242c4ce4df7
KevinZucca <kev.94@live.it>
2023-05-05 17:18:38 +0200

    feat: new Exception

--- Traits/Measurable.php
+++ Traits/Measurable.php
@@ -4,12 +4,22 @@ trait Measurable
 {
     public $storeQuantity;
 
+
     public function setQuantity($quantity)
     {
         if ($quantity == 1) {
             return $this->storeQuantity = $quantity . " piece";
+        } else if (!is_int($quantity)) {
+            throw new Exception("Write a valid number");
         } else {
             return $this->storeQuantity = $quantity . " pieces";
+        }
+
+
+        try {
+            $this->setQuantity();
+        } catch (Exception $e) {
+            echo $e;
         }
     }
 }
